071fa5fd57845382cbd5f92e073fb3432205c801
jonabasque <jonmultimedia@gmail.com>
2017-05-02 23:28:24 +0200

    Fixed relative paths in module requires

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /*index entry point*/
 const indexCSS = require('./index.css');
 
-var headerTemplate = require('components/header/header.js');
-var mainTemplate = require('components/main/main.js');
-var asideTemplate = require('components/aside/aside.js');
-var footerTemplate = require('components/footer/footer.js');
+var headerTemplate = require('./components/header/header.js');
+var mainTemplate = require('./components/main/main.js');
+var asideTemplate = require('./components/aside/aside.js');
+var footerTemplate = require('./components/footer/footer.js');
 
 var header = document.getElementById('header');
 header.innerHTML = headerTemplate;
