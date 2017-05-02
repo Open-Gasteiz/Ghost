45acb8178d4120eb288eddc632c06dff8c6a1cb6
jonabasque <jonmultimedia@gmail.com>
2017-05-02 17:40:27 +0200

    Created index.js entry point for index page

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*index entry point*/
+const headerCSS = require('components/header/header.css');
+const mainCSS = require('components/main/main.css');
+const asideCSS = require('components/aside/aside.css');
+const footerCSS = require('components/footer/footer.css');
+
+var headerTemplate = require('components/header/header.js');
+var mainTemplate = require('components/main/main.js');
+var asideTemplate = require('components/aside/aside.js');
+var footerTemplate = require('components/footer/footer.js');
+
+var header = document.getElementById('header');
+header.innerHTML = headerTemplate;
+var main = document.getElementById('main');
+main.innerHTML = mainTemplate;
+var aside = document.getElementById('aside');
+aside.innerHTML = asideTemplate;
+var footer = document.getElementById('footer');
+footer.innerHTML = footerTemplate;
