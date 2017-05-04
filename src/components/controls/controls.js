cd794f633fa1827b257f9c03e829c77d5a930d5c
jonabasque <jonmultimedia@gmail.com>
2017-05-04 04:55:15 +0200

    Created 'controls' component

---
+++ src/components/controls/controls.js
@@ -0,0 +1,10 @@
+const controlsCSS = require('./controls.css');
+
+var data = {};
+
+var controlsHTML = `<div class="buttons">
+                        <button id="leftSlide" class="controls"><</button>
+                        <button id="rightSlide" class="controls">></button>
+                    </div>`;
+
+module.exports = controlsHTML;
