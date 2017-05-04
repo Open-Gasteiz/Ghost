018a888f020dc9d49c898226355e4d33b268322f
jonabasque <jonmultimedia@gmail.com>
2017-05-04 04:56:24 +0200

    Injected 'controlsHTML' o 'headerHTML' template

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,14 @@
+const controlsHTML = require('../controls/controls');
+
 const headerCSS = require('./header.css');
 
 var data = {
     title: 'Ghost'
 };
 
-var headerHTML = `<h1>Introducción a ${data.title}</h1>`;
+var headerHTML = `
+    <h1>Taller de introducción a Ghost</h1>
+    ${controlsHTML}
+`;
 
 module.exports = headerHTML;
